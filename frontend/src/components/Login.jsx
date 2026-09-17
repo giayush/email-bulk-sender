b590f220c17214fe30a7f1e0b88e96d635dd4d85
giayush <thepryzmcreations@gmail.com>
2026-09-18 00:11:53 +0530

    fix: add credentials: 'include' to login fetch so httpOnly cookie is stored

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -18,6 +18,7 @@ function Login({ setToken }) {
         headers: {
           'Content-Type': 'application/json',
         },
+        credentials: 'include',
         body: JSON.stringify({ email, password }),
       });
 
